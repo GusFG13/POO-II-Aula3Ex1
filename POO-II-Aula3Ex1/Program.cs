888a7510b081549eaa37bf24aea0a35cba94cace
Gustavo Gonçalves <gustavofabgoncalves@gmail.com>
2023-01-18 15:11:25 -0300

    Figura classes

---
+++ POO-II-Aula3Ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace POO_II_Aula3Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
